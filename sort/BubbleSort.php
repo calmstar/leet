d5801914c3d9aa095503e2dcb6c03e287f24ea6b
chenwenxing <chenwenxing@inrice.com>
2020-01-06 16:23:27 +0800

    冒泡排序

--- sort/BubbleSort.php
+++ sort/BubbleSort.php
@@ -1,1 +1,27 @@
 <?php
+/**
+ *
+ * @param $arr
+ */
+function  bubbleSort ($arr)
+{
+    $cou = count($arr);
+    if ($cou < 2) return $arr;
+
+    for ($i = 0; $i < $cou-1; $i++) {
+        $flag = false; // 是否发生过交换，默认没有发生交换
+        for ($j = 1; $j < $cou-$i; $j++) {
+            if ($arr[$j-1] > $arr[$j]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j-1];
+                $arr[$j-1] = $temp;
+                $flag = true; // 标识改为已发生交换
+            }
+        }
+        if (!$flag) break; // 没有发生交换
+    }
+    return $arr;
+}
+
+$arr = [1, 9, 5, 6, 2, 8, 23];
+var_dump(bubbleSort($arr));
