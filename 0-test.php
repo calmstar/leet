c8f8f2d6c0e7a5f447414145ade4a7662ee3f0ec
chenwenxing <chenwenxing@inrice.com>
2020-03-17 17:54:08 +0800

    0-测试函数

--- 0-test.php
+++ 0-test.php
@@ -1,43 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xxx
- * Date: 2020/3/16
- * Time: 16:56
- */
+function buildHeap ($arr)
+{
+$cou = count($arr);
+if ($cou < 2) return $arr;
 
-function  countSortV3 ($arr) {
-    $cou = count($arr);
-    if ($cou < 2) return $arr;
-    // 仍然要得到计数数组
-    $countArr = [];
-    foreach ($arr as $v) {
-        if (!isset($countArr [$v])) {
-            $countArr[$v] = 1;
-        } else {
-            $countArr[$v]++;
-        }
-    }
-
-    // 对计数数组进行累加.先确保键是排好序的. 由于键可能不连续，使用foreach
-    // 把计数数组前面的值和现在的值加起来，赋给现在的值
-    ksort($countArr);
-    $temp = 0;
-    foreach ($countArr as $k => $v) {
-        $countArr[$k] = $countArr[$k] + $temp;
-        $temp = $countArr[$k];
-    }
-
-    $res = [];
-    // 由于要稳定排序，所以要倒着来循环放入数字
-    for ($j = $cou-1; $j >= 0; $j--) {
-        $index = --$countArr[$arr[$j]];
-        $res[$index] = $arr[$j];
-    }
-    ksort($res);
-    return $res;
+// 数组中的最后一个位置就是堆的最后一个节点
+$lastNode = $cou-1;
+// 最后一个节点的父节点就是 最后的父节点
+$lastNodeParent = intval(($lastNode-1)/2);
+// 从最后一个父节点开始向前构建堆
+for ($i = $lastNodeParent; $i >= 0; $i--) {
+heapify($arr, $cou-1, $i);
+}
+return $arr;
 }
 
-$arr = [1, 9, 5, 6, 2, 8, 23];
-$arr = [3, 3,6,5,3,6,5];
-var_dump(countSortV3($arr));
+// 每进行一次heapify,就是处理其中一棵二叉树进行堆化
+function heapify (&$arr, $lastIndex, $index)
+{
+// 防止索引$index 越界
+if ($lastIndex < $index) return ;
+
+$leftChild = 2 * $index + 1;
+$rightChild = 2 * $index + 2;
+$maxIndex = $index;
+
+// 如果左子节点的数值比父节点的数值大，则修改$maxIndex
+if ($lastIndex >= $leftChild && $arr[$maxIndex] < $arr[$leftChild]) {
+$maxIndex = $leftChild;
+}
+// 如果右子节点的数值比父节点的数值大，则修改$maxIndex
+if ($lastIndex >= $rightChild && $arr[$maxIndex] < $arr[$rightChild]) {
+$maxIndex = $rightChild;
+}
+
+// 把最大的节点树值放到父节点。
+if ($maxIndex != $index) {
+swap($arr, $index, $maxIndex);
+// 上层二叉树进行了交换，可能会导致更小的数被排下来，所以需要对$maxIndex位置进行再次堆化
+heapify($arr, $lastIndex, $maxIndex);
+}
+}
+
+function swap (&$arr, $index, $maxIndex)
+{
+$temp = $arr[$index];
+$arr[$index] = $arr[$maxIndex];
+$arr[$maxIndex] = $temp;
+}
+
+function  heapSort ($arr)
+{
+    $cou = count($arr);
+    if ($cou < 2) return $arr;
+
+    // 得到已经堆化好的数组
+    $arr = buildHeap($arr);
+    $sortArr = [];
+    for ($i = $cou-1; $i >= 0; $i--) {
+        // 把堆中顶部元素拿出（最大的数）
+        $sortArr[] = $arr[0];
+        // 把最后一个节点的元素和头部元素值交换
+        swap ($arr, 0, $i);
+        unset($arr[$i]);
+        // 对 头部元素的二叉树进行堆化
+        heapify($arr, count($arr)-1, 0);
+    }
+    return $sortArr;
+}
+
+
+$arr = [4, 10, 3, 5, 1, 9];
+//$res = buildHeap($arr);
+//var_dump($res);
+
+
+var_dump(heapSort($arr));
