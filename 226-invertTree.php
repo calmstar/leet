d6b0b6a42963d7bbd8c596acd6f8c352b9081d0a
chenwenxing <chenwenxing@inrice.com>
2020-05-29 16:23:07 +0800

    反转二叉树

---
+++ 226-invertTree.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xxx
+ * Date: 2020/5/28
+ * Time: 11:39
+ */
+
+/**
+ * 翻转一棵二叉树。
+
+示例：
+
+输入：
+
+4
+/   \
+2     7
+/ \   / \
+1   3 6   9
+输出：
+
+4
+/   \
+7     2
+/ \   / \
+9   6 3   1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/invert-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function invertTreeMy($root) {
+        if ($root === null) {
+            return $root;
+        }
+        // 错误，见：https://learnku.com/laravel/t/45157
+        $root->left = $this->invertTree($root->right);
+        $root->right = $this->invertTree($root->left);
+        return $root;
+    }
+
+
+    function invertTree($root) {
+        if ($root === null) {
+            return $root;
+        }
+        $left = $root->left;
+        $right = $root->right;
+        $root->left = $this->invertTree($right);
+        $root->right = $this->invertTree($left);
+        return $root;
+    }
+
+    function invertTreeV2 ($root) {
+        if(!isset($root)) return $root;
+
+        $tempLeftNode = $root->left;
+        $root->left = $root->right;
+        $root->right = $tempLeftNode;
+
+        $this->invertTreeV2($root->left);
+        $this->invertTreeV2($root->right);
+        return  $root;
+    }
+
+}
